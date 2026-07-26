ef7c11a5bdeb6ba7f108ff43a872121a375d8d07
Piyush Patil <piyushpatil13927@gmail.com>
2026-07-26 15:22:17 +0530

    Time: 0 ms (100%), Space: 12.4 MB (13.64%) - LeetHub

---
+++ 3894-traffic-signal-color/3894-traffic-signal-color.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def trafficSignal(self, timer):
+        if timer==0:
+            return "Green"
+        elif timer==30:
+            return "Orange"
+        elif timer>30 and timer<=90:
+            return "Red"
+        else:
+            return "Invalid"
+        

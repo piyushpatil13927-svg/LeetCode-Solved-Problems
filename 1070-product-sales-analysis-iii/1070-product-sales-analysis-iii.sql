5f4c27230be08913da6456b93e04d75c97aff18a
Piyush Patil <piyushpatil13927@gmail.com>
2026-08-27 23:11:13 +0530

    Time: 819 ms (35.38%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,1 @@
+select s.product_id, s.year as first_year, s.quantity, s.price from sales s join (select product_id, min(year) as first_year from sales group by product_id) f on s.product_id = f.product_id and s.year = f.first_year

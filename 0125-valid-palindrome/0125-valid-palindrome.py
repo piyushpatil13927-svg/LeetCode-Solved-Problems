034164e3e584bf011543e9b19f5f7831496c29f1
Piyush Patil <piyushpatil13927@gmail.com>
2026-09-10 09:18:56 +0530

    Time: 7 ms (80.82%), Space: 19.5 MB (94.4%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,7 @@
-class Solution(object):
-    def isPalindrome(self, s):
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         a=""
         for i in s:
             if i.isalnum():
                 a+=i.lower()
         return a[::-1]==a
-        
